c7d8a45b94802228af84f53dbb141f5a8cd1343a
Deepak Bhaskaran <deepak.bhaskaran@gmail.com>
2016-10-15 16:49:03 -0400

    Created a class ApiTestCase and modified all tests to use it

--- test_json_api.py
+++ test_json_api.py
@@ -1,104 +1,110 @@
 import requests
 from nose.tools import nottest
+from nose.plugins.attrib import attr
 from ConfigParser import ConfigParser
 
-test_suite_config = ConfigParser()
-test_suite_config.read('./test_suite.ini')
+class ApiTestCase():
+    main_url=''
 
-main_url = test_suite_config.get('api_settings', 'url')
-post_url = main_url + '/posts'
+    def __init__(self):
+        test_suite_config = ConfigParser()
+        test_suite_config.read('./test_suite.ini')
+        self.main_url = test_suite_config.get('api_settings', 'url')
 
+    def get_results(self, url):
+        r = requests.get(self.main_url + url)
+        return (r.json(), r.status_code)
+
+@attr('smoke')
 def test_get_one_post():
     '''
     Get one post successfully.
     :return:
     '''
-    post_url = main_url + '/posts/1'
-    r = requests.get(post_url)
-    assert r.status_code == 200
+    test_case = ApiTestCase()
+    (result, response_code) = test_case.get_results('/posts/1')
+    assert response_code == 200
 
 def test_get_posts():
     '''
     Get /posts without arguments, get default number of posts = 100
     :return:
     '''
-    post_url = main_url + '/posts'
-    r = requests.get(post_url)
-    data = r.json()
-    assert len(data) == 100
-    assert r.status_code == 200
+    test_case = ApiTestCase()
+    (result, response_code) = test_case.get_results('/posts')
+    assert len(result) == 100
+    assert response_code == 200
 
 def test_get_page_of_posts():
     '''
     Get posts on given page
     :return:
     '''
-    post_url = main_url + '/posts?_page=7'
-    r = requests.get(post_url)
-    assert r.status_code == 200
+    test_case = ApiTestCase()
+    (result, response_code) = test_case.get_results('/posts?_page=7')
+    assert response_code == 200
 
 def test_get_slice_of_posts():
     '''
     Get a slice of posts
     :return:
     '''
-    post_url = main_url + '/posts?_start=20&_end=30'
-    r = requests.get(post_url)
-    data = r.json()
-    assert data[0]['id'] == 21
-    assert data[-1]['id'] == 30
-    assert r.status_code == 200
+    test_case = ApiTestCase()
+    (result, response_code) = test_case.get_results('/posts?_start=20&_end=30')
+    assert response_code == 200
+    assert result[0]['id'] == 21
+    assert result[-1]['id'] == 30
 
 def test_get_comments_on_post():
     '''
     Get comments from a post
     :return:
     '''
-    post_url = main_url + '/posts/1/comments'
-    r = requests.get(post_url)
-    assert r.status_code == 200
+    test_case = ApiTestCase()
+    (result, response_code) = test_case.get_results('/posts/1/comments')
+    assert response_code == 200
 
 def test_get_page_of_albums():
     '''
     Get albums on given page
     :return:
     '''
-    post_url = main_url + '/albums?_page=7'
-    r = requests.get(post_url)
-    assert r.status_code == 200
+    test_case = ApiTestCase()
+    (result, response_code) = test_case.get_results('/albums?_page=7')
+    assert response_code == 200
 
 def test_get_page_of_photos():
     '''
     Get photos on given page
     :return:
     '''
-    post_url = main_url + '/photos?_page=7'
-    r = requests.get(post_url)
-    assert r.status_code == 200
+    test_case = ApiTestCase()
+    (result, response_code) = test_case.get_results('/photos?_page=7')
+    assert response_code == 200
 
 def test_get_page_of_users():
     '''
     Get users on given page
     :return:
     '''
-    post_url = main_url + '/users?_page=7'
-    r = requests.get(post_url)
-    assert r.status_code == 200
+    test_case = ApiTestCase()
+    (result, response_code) = test_case.get_results('/users?_page=7')
+    assert response_code == 200
 
 def test_get_one_todo():
     '''
     Get one todo
     :return:
     '''
-    post_url = main_url + '/todos/1'
-    r = requests.get(post_url)
-    assert r.status_code == 200
+    test_case = ApiTestCase()
+    (result, response_code) = test_case.get_results('/todos/1')
+    assert response_code == 200
 
 def test_get_page_of_todos():
     '''
     Get todos on given page
     :return:
     '''
-    post_url = main_url + '/todos?_page=7'
-    r = requests.get(post_url)
-    assert r.status_code == 200
+    test_case = ApiTestCase()
+    (result, response_code) = test_case.get_results('/todos?_page=7')
+    assert response_code == 200
